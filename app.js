488a94ba1d4467be75ef4a7f03869617a0ac505e
kongchao <1871170192@qq.com>
2023-02-09 16:08:16 +0800

    '最终配置'

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-var express = require("express");
+import express from 'express'
+import path from  "path"
+import {fileURLToPath} from 'url';
+import bodyParser from 'body-parser'
+//创建数据库连接对象
+import mysql from 'mysql'
+import {underline2Hump,jsonToUnderline} from './util.js'
+import dotenv from 'dotenv'
 var app = express();
-var path = require("path");
-var bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-var jsonToUnderline = require("./util").jsonToUnderline;
+app.use(bodyParser.urlencoded());
+// var jsonToUnderline = require("./util").jsonToUnderline;
 //获取process.env插件
-require("dotenv").config({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({
   path: path.resolve(__dirname, `./environments/${process.env.NODE_ENV}.env`),
 });
 //设置跨域访问
@@ -16,12 +22,11 @@ app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By", " 3.2.1");
-  res.header("Content-Type", "application/jsoncharset=utf-8");
+  res.header("Content-Type", "application/json;charset=utf-8");
   next();
 });
 
-//创建数据库连接对象
-const mysql = require("mysql");
+
 const { host, user, password, database, multipleStatements } = process.env;
 const conn = mysql.createConnection({
   host,
@@ -43,26 +48,27 @@ conn.connect(function (err) {
 //全部列表查询
 app.get("/api/test", function (req, res) {
   const occur_period = req?.query?.occurPeriod || "";
-  console.log("req :: ", req);
   let sqlQuery =
     "select * from nanshan_economic_small.small_accounting_indices where occur_period = ?";
   conn.query(sqlQuery, occur_period, (err, results) => {
-    console.log("err :: ", err);
-    console.log("results :: ", results);
     if (err)
-      return res.json({ err_code: 1, message: "暂无数据", affextedRows: 0 });
+      return res.json({ code: 1, message: "暂无数据", affextedRows: 0 });
     res.json({
-      err_code: 200,
-      message: jsonToUnderline(results),
+      code: 200,
+      data: jsonToUnderline(results),
+      message:'查询成功！',
       affextedRows: results.affextedRows,
     });
   });
-  // res.status(200),
+
+  // res.status(200)
   // res.json(questions)
 });
 
+app.route('/', (req, res) => res.send('Hello World!'))
+
 //配置服务端口
-var server = app.listen("3004", function () {
+var server = app.listen("3303", function () {
   var host = server.address().address;
 
   var port = server.address().port;
--- util.js
+++ util.js
@@ -1,12 +1,12 @@
 // 字符串的下划线格式转驼峰格式，eg：hello_world => helloWorld
-function underline2Hump(s) {
+export const  underline2Hump=(s) =>{
   return s.replace(/_(\w)/g, function(all, letter) {
     return letter.toUpperCase()
   })
 }
 
 // JSON对象的key值转换为下划线格式
-function jsonToUnderline(obj) {
+export const  jsonToUnderline=(obj)=> {
   if (obj instanceof Array) {
     obj.forEach(function(v, i) {
       jsonToUnderline(v)
@@ -23,5 +23,3 @@ function jsonToUnderline(obj) {
   }
   return obj
 }
-
-module.exports = {jsonToUnderline} 
